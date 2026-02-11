afb17f9fe020befe7b7667aa93f5647b28f28006
waroon <Vwin2537@gmail.com>
2026-02-11 18:41:55 +0700

    Fix: Enable pdo_mysql extension and remove redundant init command

--- config/database.php
+++ config/database.php
@@ -26,7 +26,6 @@ function getDB(): PDO {
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . DB_CHARSET,
         ];
         
         try {
